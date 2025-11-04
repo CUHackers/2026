d5835d53afd3bf76f8e49da7f8d35c797d650165
Malia Fairbanks <Malia.Fairbanks7@gmail.com>
2025-11-04 17:11:40 -0500

    trying
    trying

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // Change REPO_NAME to your repo if deploying to a project page.
 // For root pages (username.github.io), set base to '/'.
 const isProjectPage = true // set to false if deploying to username.github.io root
-const repoName = 'repo-name' // <-- change this
+const repoName = '2026' // <-- change this
 
 export default defineConfig({
   plugins: [react()],
-  base: isProjectPage ? `/${repoName}/` : '/'
+  base: isProjectPage ? `/${2026}/` : '/'
 })
