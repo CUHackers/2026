23147ee3df9c2dfcd84e064f79ed297b7aac591d
Malia Fairbanks <Malia.Fairbanks7@gmail.com>
2025-11-04 16:56:15 -0500

    kk

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Change REPO_NAME to your repo if deploying to a project page.
+// For root pages (username.github.io), set base to '/'.
+const isProjectPage = true // set to false if deploying to username.github.io root
+const repoName = 'repo-name' // <-- change this
+
+export default defineConfig({
+  plugins: [react()],
+  base: isProjectPage ? `/${repoName}/` : '/'
+})
